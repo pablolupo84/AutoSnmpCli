2d85fccd0f7861cbe94c77b37affa7abbf9e4901
Pablo Lupo <pablolupo84@gmail.com>
2022-05-23 13:41:32 -0300

    listen traps

--- clases/SnmpCliApp.py
+++ clases/SnmpCliApp.py
@@ -6,7 +6,7 @@ import LogApp
 
 
 class SnmpCliApp():
- 
+    
     dic_configuracion={}
 
     OID_reference = {
@@ -26,13 +26,16 @@ class SnmpCliApp():
     }
 
     def __init__(self):
-        self.logApp=LogApp.LogAppFile("snmtp_log.txt")
+        self.logApp=LogApp.LogAppFile("../snmtp_log.txt")
+        #self.logApp=LogApp.LogAppOutput()
         self.snmpEngine= engine.SnmpEngine()
         self.datosConfigIni=FileConfigIni.FileConfigIni()
 
         self.InicializarDatos()
+        self.listenTraps()
 
     def InicializarDatos(self):
+        self.logApp.writeLog('------------------------------------------------------------------------------')
         self.logApp.writeLog('Inicializando Datos - Config.ini')
         self.dic_configuracion.setdefault('TrapAgentAddress',self.datosConfigIni.showValueItem('TRANSPORT_SETUP','TrapAgentAddress'))
         self.logApp.writeLog('TrapAgentAddress: {}'.format(self.dic_configuracion.get('TrapAgentAddress')))
@@ -56,7 +59,50 @@ class SnmpCliApp():
                 value_OID=self.OID_reference.get(val.prettyPrint())
 
             self.logApp.writeLog('Name: {} | Val: {}'.format(name_OID,value_OID))
+    
+    def listenTraps(self):
 
+        self.logApp.writeLog('Iniciando Auto_SNMPTrapReceiver')
+        try:
+            
+            if self.dic_configuracion:
+                self.logApp.writeLog('UDP over IPv4, first listening interface/port')
+                config.addTransport(
+                    self.snmpEngine,
+                    udp.domainName + (1,),
+                    udp.UdpTransport().openServerMode((self.dic_configuracion.get('TrapAgentAddress'),
+                                                        self.dic_configuracion.get('Port')))
+                )
 
-test=SnmpCliApp()
-print(len(test.dic_configuracion))
+                # SNMPv1/2c setup
+                self.logApp.writeLog('SNMPv1/2c setup')
+                config.addV1System(self.snmpEngine, self.dic_configuracion.get('CommunityName'), 
+                                                    self.dic_configuracion.get('ModeCommunity'))
+            else:
+                self.logApp.writeLog('Inicializar Datos - Vacio por datos sin configurar')
+
+        except Exception as err: 
+            self.logApp.writeLog("Error: {}".format(err))
+        else: 
+            
+            self.logApp.writeLog('Start SNMPTrapReceiver')
+            self.logApp.writeLog("Agent is listening SNMP Trap on {} Port : {}".format(self.dic_configuracion.get('TrapAgentAddress'),
+                                self.dic_configuracion.get('Port')))
+            ntfrcv.NotificationReceiver(self.snmpEngine, self.cbFun)
+            self.logApp.writeLog('Register SNMP Application at the SNMP engine')
+
+            self.snmpEngine.transportDispatcher.jobStarted(1)  
+            self.logApp.writeLog('this job would never finish: snmpEngine.transportDispatcher.jobStarted()')
+
+            try:
+                self.snmpEngine.transportDispatcher.runDispatcher()
+                self.logApp.writeLog('snmpEngine.transportDispatcher.runDispatcher()')
+            except:
+                self.snmpEngine.transportDispatcher.closeDispatcher()
+                self.logApp.writeLog('snmpEngine.transportDispatcher.closeDispatcher()')
+
+        finally:
+            self.logApp.writeLog('Finaliza app Auto_SNMPTrapReceiver')
+
+# test=SnmpCliApp()
+# print(len(test.dic_configuracion))
