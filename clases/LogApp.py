00f551ccb4ab9de08ffdccbadcb01d4c21a12094
Pablo Lupo <pablolupo84@gmail.com>
2022-05-23 10:10:39 -0300

    Creacion clase LogApp

--- clases/LogApp.py
+++ clases/LogApp.py
@@ -1,39 +1,6 @@
-"""
-from abc import ABCMeta, abstractmethod
-
-
-class Animal(metaclass=ABCMeta):
-    @abstractmethod
-    def eat(self): pass
-
-    @abstractmethod
-    def sleep(self): pass
-
-    def breathe(self):
-                 print ('Respirando aire')
-
-
-class Dog(Animal):
-    def eat(self):
-                 print ('Come huesos')
-
-    def sleep(self):
-                 print ('perrera para dormir')
-
-
-class Cat(Animal):
-    def eat(self):
-                 print ('Come pescado')
-
-    def sleep(self):
-                 print ('Casa del gato dormido')
-
-                 from abc import ABCMeta, abstractmethod"""
-
-#!/usr/bin/env python
-
 import os
 from datetime import datetime
+from abc import ABCMeta, abstractmethod
 
 """
 Descripcion: Modulo que permite manipular el Log de Errores de la APlicacion.
@@ -42,20 +9,44 @@ Autor: Autotrol PL - Sistemas
 Version: 0.1
 """
 
-class logApp:
-    """Modulo que permite manejar archivo de configuracion"""    
+class LogApp(metaclass=ABCMeta):
+         
+    @abstractmethod
     def __init__(self,logFile):
+        pass
+
+    @abstractmethod
+    def writeLog(self,msgError):
+        pass
+
+class LogAppFile(LogApp):
+    """Clase que permite manejar un log en un archivo"""
+    
+    def __init__(self,logFile="log_file.txt"):
         self.logFile = logFile
         self.logApp=open(self.logFile,'a+')
         self.logApp.close()
-
+    
     def writeLog(self,msgError):
+        
         self.msgError=str(datetime.now()) + ":" + msgError + "\n"
         self.logApp=open(self.logFile,'a+')
         self.logApp.write(self.msgError)
         self.logApp.close()
 
+class LogAppOutput(LogApp):
+    """Clase que permite manejar un log por Standar Output"""
+    
+    def __init__(self):
+        print("Iniciando salida por Standar Output")
+    
+    def writeLog(self,msgError):
+        self.msgError=str(datetime.now()) + ":" + msgError + "\n"
+        print(self.msgError)
+
 # TEST log_file.py
 # mensajeError="ESte es un nuevo mensaje de error"
-# fileError=logApp("log_file.txt")
+# fileError=LogAppFile()
+# fileError.writeLog(mensajeError)
+# fileError=LogAppOutput()
 # fileError.writeLog(mensajeError)
